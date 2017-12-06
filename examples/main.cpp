d1bc50904de3f766e555264cfc065ac90be5d417
IanH <ian_web1@hubbertz.de>
2017-12-06 22:14:41 +0100

    Fixed main example to support new ctor

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,14 +4,14 @@
 #include <LoggerNode.h>
 
 #define FW_NAME "homie-wx2812fx"
-#define FW_VERSION "0.0.3"
+#define FW_VERSION "0.0.4"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 /* End of magic sequence for Autodetectable Binary Upload */
 
-WS2812Node ws2812Node;
+WS2812Node ws2812Node("ws2812fx");
 
 void setup() {
   Homie_setFirmware(FW_NAME, FW_VERSION);
