e5ce24280ab8108d5e6e04582b3db04c238621fa
Ian Hubbertz <ian_web1@hubbertz.de>
2019-09-16 23:16:55 +0200

    Updated examples

--- examples/simple2.cpp
+++ examples/simple2.cpp
@@ -4,15 +4,22 @@
 #include <LoggerNode.h>
 
 #define FW_NAME "homie-wx2812fx"
-#define FW_VERSION "0.1.4"
+#define FW_VERSION "0.3.11"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 /* End of magic sequence for Autodetectable Binary Upload */
 
-WS2812Node ws2812Node("Lampe",  FX_MODE_FIRE_FLICKER_SOFT);		// first WS2812 Strip with PIN loaded from config.json
-WS2812Node ws2812Node2("Feuer", FX_MODE_FIRE_FLICKER_INTENSE, 2);	// second WS2812 Strip with custom pin (2 == D4 (Wemos))
+// first WS2812 Strip with PIN loaded from config.json
+//WS2812Node ws2812Node("Feuer1",  FX_MODE_FIRE_FLICKER_SOFT,NEO_GRB + NEO_KHZ800, -1, 8);
+
+// second WS2812 Strip with custom pin (2 == D4 (Wemos))
+//WS2812Node ws2812Node2("Feuer2", FX_MODE_FIREWORKS_RANDOM, NEO_GRB + NEO_KHZ800, 2, 8);
+
+// third WS2812 Strip with custom pin (4 == D2 (Wemos))
+WS2812Node ws2812Node3("strip", "1st Strip", FX_MODE_CIRCUS_COMBUSTUS, NEO_GRBW + NEO_KHZ800, 4, 429);
+LoggerNode LN;
 
 void setup() {
   Serial.begin(74880);
@@ -27,5 +34,11 @@ void setup() {
 }
 
 void loop() {
+	static uint32_t last = 0;
 	Homie.loop();
+	if ((last + 5000) < millis()) {
+		last = millis();
+		LN.logf(__PRETTY_FUNCTION__, LoggerNode::DEBUG, "Free heap: %d bytes", ESP.getFreeHeap());
+	}
+
 }
